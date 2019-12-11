cd24e181be35d7915154896751bd2d40bcc02206
kribajt <kribajt@o2.pl>
2019-12-11 21:24:00 +0100

    dodano wyswietlanie pojedynczego wpisu

--- templates/job-single.php
+++ templates/job-single.php
@@ -4,6 +4,18 @@
 <small>
 Posted By <?php echo $job->contact_user; ?> on <?php echo $job->post_date; ?>
 </small>
+<hr>
+<p class="lead"><?php echo $job->description; ?></p>
+<ul class="list-group">
+    <li class="list-group-item"><strong>Firma: </strong><?php echo $job->company; ?></li>
+    <li class="list-group-item"><strong>Stawka: </strong><?php echo $job->salary; ?></li>
+    <li class="list-group-item"><strong>Email: </strong><?php echo $job->contact_email; ?></li>
+</ul>
+<br><br>
+<a href="index.php">Go Back</a>
+<br><br>
+
+
 <?php include 'inc/header.php'; ?>
 
 
