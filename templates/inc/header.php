1317117788ceeb351000a4df1536ae5fad7c6426
kribajt <kribajt@o2.pl>
2019-12-11 19:12:06 +0100

    dodano zmiany zmienne logujace do bazy

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>JobList</title>
+    <title><?php echo SITE_TITLE; ?></title>
     <link rel="stylesheet" href="https://bootswatch.com/4/solar/bootstrap.min.css">
 <link rel="stylesheet" href="css/style.css">
 </head>
@@ -17,12 +17,9 @@
               <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+              <a class="nav-link" href="#">Create Listing</a>
             </li>
           </ul>
         </nav>
-        <h3 class="text-muted">Project name</h3>
+        <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
       </div>
